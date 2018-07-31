e6f3d7f5d2495c1ae10ae890d87dfdf1564da53f
iwamoto <iwamoto@cshool.jp>
2018-07-31 14:52:54 +0900

    DeviceをUA依存のものに変更

--- Device.js
+++ Device.js
@@ -1,20 +1,44 @@
-import $ from 'jquery';
-import { BREAK_POINT } from '../config';
+const UA = window.navigator.userAgent.toLocaleLowerCase();
 
-export default class Device {
+class Device {
   /**
-   * PCか判定.
-   * @returns {boolean}p
-   */
-  static isPc() {
-    return $(window).width() > BREAK_POINT;
-  }
-
-  /**
-   * SPか判定.
+   * モバイル判定
    * @returns {boolean}
    */
   static isMobile() {
-    return $(window).width() <= BREAK_POINT;
+    return (
+      UA.indexOf('iphone') >= 0 ||
+      UA.indexOf('ipod') >= 0 ||
+      UA.indexOf('android') >= 0 && UA.indexOf('mobile') >= 0
+    );
+  }
+
+  /**
+   * タブレット判定
+   * @returns {boolean}
+   */
+  static isTablet() {
+    return UA.indexOf("ipad") >= 0 ||
+      (UA.indexOf("android") >= 0 && UA.indexOf("mobile") < 0);
+  }
+
+  /**
+   * safari判定
+   * @returns {boolean}
+   */
+  static isSafari() {
+    return UA.indexOf('safari') >= 0 && UA.indexOf('chrome') < 0;
+  }
+
+  /**
+   * IE、Edge判定
+   * @returns {boolean}
+   */
+  static isMsBrowser() {
+    return UA.indexOf('msie') >= 0 ||
+      UA.indexOf('trident') >= 0 ||
+      UA.indexOf('edge') >= 0;
   }
 }
+
+export default Device;
